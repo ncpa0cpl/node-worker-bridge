993afa999cfbf6b4ed8fc9e86f50c54a1c84e93e
ncpa0cpl <sbretner@touchbistro.com>
2021-10-04 17:13:01 +0200

    test: jest config

--- __tests__/helpers.d.ts
+++
@@ -1,6 +0,0 @@
-export declare const testPromise: <T>() => {
-    wait: Promise<T>;
-    resolve: (value: T | PromiseLike<T>) => void;
-    reject: (reason?: any) => void;
-};
-export declare function sleep(ms: number): Promise<void>;
--- jest.config.js
+++ jest.config.js
@@ -1,5 +1,30 @@
 /** @type {import('ts-jest/dist/types').InitialOptionsTsJest} */
 module.exports = {
-  preset: 'ts-jest',
-  testEnvironment: 'node',
+  preset: "ts-jest",
+  testRegex: ".*__tests__/.+(\\.test\\.(ts|js|tsx|jsx))$",
+  transform: {
+    "^.+\\.(ts|tsx)$": "ts-jest",
+  },
+  testEnvironment: "jsdom",
+  roots: ["<rootDir>"],
+  collectCoverageFrom: ["src/**/*.ts"],
+  coverageReporters: ["html", "text"],
+  coverageThreshold: {
+    global: {
+      branches: 0,
+      functions: 0,
+      lines: 0,
+      statements: 0,
+    },
+  },
+  coveragePathIgnorePatterns: [
+    "/node_modules/",
+    "/coverage/",
+    "/__mocks__/",
+    "/__tests__/",
+    "/dist/",
+    "/scripts/",
+    "/.husky/",
+    "/.vscode/",
+  ],
 };
